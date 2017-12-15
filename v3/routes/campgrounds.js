20d6cff8a4c248c244d492914fa5b1b6621067ea
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-15 15:33:08 -0500

    attached user to newly created campgrounds

--- v3/routes/campgrounds.js
+++ v3/routes/campgrounds.js
@@ -36,7 +36,11 @@ router.post("/", isLoggedIn, function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var description = req.body.description;
-	var newCampground = {name: name, image: image, description: description};
+	var author = {
+			id: req.user._id,
+			username: req.user.username
+	}
+	var newCampground = {name: name, image: image, description: description, author: author};
 	// create new campground and save to DB
 	Campground.create(newCampground, function(err, newlyCreated){
 		if(err) {
