fd16ea8a0ee3a5d51b3fb009be38ae87578e7c49
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-27 15:10:23 -0500

    added delete comment functionality

--- v3/routes/comments.js
+++ v3/routes/comments.js
@@ -79,6 +79,18 @@ router.put("/:comment_id", function(req, res) {
 	});
 });
 
+// DESTROY COMMENT
+router.delete("/:comment_id", function(req, res) {
+	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+		if(err) {
+			console.log(err);
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	})
+})
+
 
 module.exports = router;
 
