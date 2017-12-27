b8322ebcf65df3f7c00524ed6bca31479fcd7330
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-27 15:23:08 -0500

    added authentication around comment update and destroy

--- v3/routes/comments.js
+++ v3/routes/comments.js
@@ -12,6 +12,25 @@ function isLoggedIn(req, res, next) {
 	res.redirect("/login");
 }
 
+function isCommentOwner(req, res, next) {
+	 if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+               res.redirect("back");
+           } else {
+               // does user own the campground?
+            	if(foundComment.author.id.equals(req.user._id)) {
+              	  next();
+            	} else {
+                	res.redirect("back");
+            	}
+           }
+        });
+    } else {
+        res.redirect("back");
+    }
+}	
+
 
 //=================================================================
 // ROUTES
@@ -56,7 +75,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 // EDIT COMMENT
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", isCommentOwner, function(req, res) {
 	Comment.findById(req.params.comment_id, function(err, foundComment){
 		if(err) {
 			console.log(err);
@@ -68,7 +87,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // UPDATE COMMENT
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", isCommentOwner, function(req, res) {
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
 		if(err) {
 			console.log(err);
@@ -80,7 +99,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // DESTROY COMMENT
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", isCommentOwner, function(req, res) {
 	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
 		if(err) {
 			console.log(err);
